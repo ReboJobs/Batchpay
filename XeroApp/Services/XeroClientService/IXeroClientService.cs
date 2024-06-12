122e76bdd6a3f0b08e01afb46f8bbd45520dc1e3
ReboJobs <wajamo25@gmail.com>
2024-06-12 18:57:29 +0800

    Add files via upload

---
+++ XeroApp/Services/XeroClientService/IXeroClientService.cs
@@ -0,0 +1,14 @@
+﻿using Core;
+using XeroApp.Models.BusinessModels;
+
+namespace XeroApp.Services.XeroClientService
+{
+    public interface IXeroClientService
+    {
+        Task<XeroClientModel> InsertClientTrackAsync(XeroClientModel model);
+
+        Task<XeroClientModel> SearchClientAsync(string clientUniqueID);
+    }
+}
+
+
---
+++ XeroApp/Services/XeroClientService/XeroClientService.cs
@@ -0,0 +1,68 @@
+﻿using XeroApp.Models.BusinessModels;
+using Persistence.Config;
+using AutoMapper;
+using Core;
+
+
+namespace XeroApp.Services.XeroClientService
+{
+    public class XeroClientService : IXeroClientService
+    {
+        private readonly XeroAppDbContext _db;
+        private readonly IMapper _mapper;
+        private readonly IConfiguration _config;
+
+        public XeroClientService(
+            XeroAppDbContext db,
+            IMapper mapper,
+            IConfiguration config)
+        {
+            _db = db;
+            _mapper = mapper;
+            _config = config;
+        }
+
+        public async Task<XeroClientModel> InsertClientTrackAsync(XeroClientModel model)
+        {
+            var clientDB = _db.XeroClient.FirstOrDefault(x => x.XeroClientUniqueId == model.XeroClientUniqueId);
+
+            if (clientDB != null)
+            {
+                // NOT Implement yet
+            }
+            else
+            {
+                clientDB = new XeroClientDataModel
+                {
+                   XeroClientUniqueId = model.XeroClientUniqueId,
+                   IsActive = model.IsActive,
+                   Name = model.XeroClientUniqueId,
+                };
+
+                _db.XeroClient.Add(clientDB);
+            }
+
+            await _db.SaveChangesAsync();
+            model.Id = clientDB.Id;
+
+            return model;
+        }
+
+
+        public async Task<XeroClientModel> SearchClientAsync(string clientUniqueID)
+        {
+            var tenant = _db.XeroClient.Where(x => x.XeroClientUniqueId.Equals(clientUniqueID))
+                        .Select(x => new XeroClientModel
+                        {
+                            Id = x.Id,
+                            XeroClientUniqueId = x.XeroClientUniqueId,
+                            Name = x.XeroClientUniqueId,
+                            IsActive = x.IsActive,
+                        }).FirstOrDefault();
+
+            return tenant;
+
+        }
+
+    }
+}
