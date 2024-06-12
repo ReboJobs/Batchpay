d007b923723505b67df226fb528a8adb9dc4232e
ReboJobs <wajamo25@gmail.com>
2024-06-12 18:55:27 +0800

    Add files via upload

---
+++ XeroApp/Services/SubscriptionService/ISubscriptionService.cs
@@ -0,0 +1,12 @@
+﻿using Core;
+using XeroApp.Models.BusinessModels;
+
+namespace XeroApp.Services.SubscriptionService
+{
+    public interface ISubscriptionService
+    {
+        Task<XeroUserTenantsSubscriptionModel> InsertTenantSubscriptionAsync(XeroUserTenantsSubscriptionModel model);
+
+        Task<XeroUserTenantsSubscriptionModel> SearchTenantSubscriptionAsync(string tenantUniqueID);
+    }
+}
---
+++ XeroApp/Services/SubscriptionService/SubscriptionService.cs
@@ -0,0 +1,49 @@
+﻿using XeroApp.Models.BusinessModels;
+using Persistence.Config;
+using AutoMapper;
+using Core;
+
+
+namespace XeroApp.Services.SubscriptionService
+{
+    public class SubscriptionService : ISubscriptionService
+    {
+
+        private readonly XeroAppDbContext _db;
+        private readonly IMapper _mapper;
+        private readonly IConfiguration _config;
+
+        public SubscriptionService(
+            XeroAppDbContext db,
+            IMapper mapper,
+            IConfiguration config)
+        {
+            _db = db;
+            _mapper = mapper;
+            _config = config;
+        }
+
+        public Task<XeroUserTenantsSubscriptionModel> InsertTenantSubscriptionAsync(XeroUserTenantsSubscriptionModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<XeroUserTenantsSubscriptionModel> SearchTenantSubscriptionAsync(string tenantUniqueID)
+        {
+            var tenantSubscriptions = (from tenSubs in _db.XeroUserTenantsSubscription
+                                            join tenants in _db.XeroTenant on tenSubs.XeroTenantId equals tenants.Id
+                                            where tenants.XeroTenantUniqueId == tenantUniqueID && tenSubs.IsActive == true
+                                            select new XeroUserTenantsSubscriptionModel
+                                            {
+                                                Id = tenSubs.Id,
+                                                XeroClientId = tenSubs.XeroClientId,
+                                                SubscriptionPlanId = tenSubs.SubscriptionPlanId,    
+                                                XeroTenantId = tenSubs.XeroTenantId,
+                                                DateStart = tenSubs.DateStart,
+                                                DateEnd = tenSubs.DateEnd,
+                                                IsActive = tenSubs.IsActive,
+                                            }).FirstOrDefault();
+            return tenantSubscriptions;
+        }
+    }
+}
